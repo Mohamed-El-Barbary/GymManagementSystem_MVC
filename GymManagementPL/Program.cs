946bd1c4414ff2b10c37ad29abad281061370960
Mohamed-El-Barbary <mohamedelbarbary511@gmail.com>
2025-11-04 03:45:59 +0200

    Added registration of all services in the DI container (Program.cs)

--- GymManagementPL/Program.cs
+++ GymManagementPL/Program.cs
@@ -25,6 +25,11 @@ namespace GymManagementPL
             builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
             builder.Services.AddScoped<ISessionRepository, SessionRepository>();
             builder.Services.AddScoped<IAnalyticsService, AnalyticsService>();
+            builder.Services.AddScoped<IMemberService, MemberService>();
+            builder.Services.AddScoped<ITrainerService, TrainerService>();
+            builder.Services.AddScoped<IPlanService, PlanService>();
+            builder.Services.AddScoped<ISessionService, SessionService>();
+
             builder.Services.AddAutoMapper(x => x.AddProfile(new MappingProfiles()));
 
             
